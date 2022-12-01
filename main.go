18cdbdda78cabbdd9fa701ce0c55f262a412e09a
Zhengqi Xi <zhengqixi@gmail.com>
2022-11-30 21:55:22 -0500

    Do check

--- main.go
+++ main.go
@@ -15,41 +15,37 @@ type Message struct {
 	Timestamp time.Time
 }
 
+// noEncryptor is a simple function that does not do any encryption
 func noEncryptor(data []byte) []byte {
 	return data
 }
 
+// Returns 2 function: (encryptor, decryptor)
+// encryptor encrypts a slice of bytes
+// decryptor decrypts it
+// Things like the cipher and nonce are curried in
 func encryptionFunc() (func([]byte) []byte, func([]byte) []byte) {
 	key := make([]byte, 32)
 	if _, err := rand.Read(key); err != nil {
 		panic(err)
 	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := rand.Read(nonce); err != nil {
+		panic(err)
+	}
 	encrypt := func(data []byte) []byte {
-		block, err := aes.NewCipher(key)
-		if err != nil {
-			panic(err)
-		}
-		gcm, err := cipher.NewGCM(block)
-		if err != nil {
-			panic(err)
-		}
-		nonce := make([]byte, gcm.NonceSize())
-		if _, err := rand.Read(nonce); err != nil {
-			panic(err)
-		}
 		return gcm.Seal(nil, nonce, data, nil)
 	}
 	decrypt := func(encrypted []byte) []byte {
-		block, err := aes.NewCipher(key)
-		if err != nil {
-			panic(err)
-		}
-		gcm, err := cipher.NewGCM(block)
-		if err != nil {
-			panic(err)
-		}
-		nonceSize := gcm.NonceSize()
-		decrypted, err := gcm.Open(nil, encrypted[:nonceSize], encrypted[nonceSize:], nil)
+		decrypted, err := gcm.Open(nil, nonce, encrypted, nil)
 		if err != nil {
 			panic(err)
 		}
@@ -59,6 +55,7 @@ func encryptionFunc() (func([]byte) []byte, func([]byte) []byte) {
 
 }
 
+// sender is a function that represents the source
 func sender(out chan []byte, max int, encryptor func(data []byte) []byte) {
 	timestamp := time.Now()
 	for i := 0; i < max; i += 1 {
@@ -70,16 +67,23 @@ func sender(out chan []byte, max int, encryptor func(data []byte) []byte) {
 		if err != nil {
 			panic(err)
 		}
-		out <- payload
-		timestamp.Add(time.Second)
+		encrypted := encryptor(payload)
+		out <- encrypted
+		timestamp = timestamp.Add(time.Second)
 	}
 	close(out)
 }
 
-func noDuplicate() bool {
-	return false
+// createRandomDuplicate returns a function which randomly returns true
+func createRandomDuplicate(prob int) func() bool {
+	return func() bool {
+		// TODO: Implement some random logic based on passed in probability
+		// If 0, should never return true
+		return false
+	}
 }
 
+// router is a function representing the network router
 func router(fromSender chan []byte, toReceiver chan []byte, duplicate func() bool) {
 	for data := range fromSender {
 		toReceiver <- data
@@ -90,6 +94,9 @@ func router(fromSender chan []byte, toReceiver chan []byte, duplicate func() boo
 	close(toReceiver)
 }
 
+// getReceiver creates a receiver function
+// We have a function create a function to account for the control case
+// Where we don't check a timestamp at all
 func getReceiver(in chan []byte, decryptor func(data []byte) []byte) func() int {
 	if decryptor == nil {
 		return func() int {
@@ -124,12 +131,18 @@ func getReceiver(in chan []byte, decryptor func(data []byte) []byte) func() int 
 }
 
 func main() {
+	// TODO: Take some arguments to vary the
+	// test variables
+	// maxInt is the state we want to check
 	maxInt := 1000
+	// senderToRoute represents the network queue between the sender and the router
 	senderToRoute := make(chan []byte, 1024)
+	// routeToReceiver represents the network queue between the router and the receiver
 	routeToReceiver := make(chan []byte, 1024)
 	encrypt, decrypt := encryptionFunc()
 	receiver := getReceiver(routeToReceiver, decrypt)
-	go router(senderToRoute, routeToReceiver, noDuplicate)
+	duplicateProb := createRandomDuplicate(0)
+	go router(senderToRoute, routeToReceiver, duplicateProb)
 	start := time.Now()
 	// The sender, we're going
 	go sender(senderToRoute, maxInt, encrypt)
